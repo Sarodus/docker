2e18ade2933d0f45d0a412a8a1e1144230515140
Jordi Avella <jordiavellapol@gmail.com>
2016-12-28 18:51:12 +0100

    sanic to submodule, step 1

--- www/sanic_test/app/config.py
+++
@@ -1,4 +0,0 @@
-from sanic.config import Config
-
-class MyConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:////data/sql.db"
--- www/sanic_test/app/views/api.py
+++
@@ -1,9 +0,0 @@
-from sanic.response import json
-from sanic.views import HTTPMethodView
-from app.ext.api_model.sqla import ModelView
-from ..db import Session
-from ..models import User
-
-
-def register_routes(app):
-    ModelView(User, Session).register(app, url_prefix='/user')
