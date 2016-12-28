e305320e9a6aa46afe42b6b04959078e9f4af06f
Jordi Avella <jordiavellapol@gmail.com>
2016-12-28 18:57:57 +0100

    creator app to submodule, step 1

--- www/creator/app/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from .base import Design, Creation, Page, Component
--- www/creator/app/views/component/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import Component
-
-from .media import Media
-# from .news import News
-from .twitter import Twitter
-# from .html import Html
-# from .youtube import Youtube
-# from .poll import Poll
